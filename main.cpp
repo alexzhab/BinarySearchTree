ce77e1b48f810f1a1329a8d98d882f5ba4d2c71b
Zhabitskaya Alexandra <yasashochek@gmail.com>
2024-10-04 16:45:23 +0300

    enh: added small tests to main

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 #include <iostream>
 
 int main() {
+    TreeNode<int> node1(5);
+    TreeNode<int> node2(4);
+    node1.set_lchild(&node2);
+
+    TreeNode<int> node3(1);
+    node3.set_lchild(&node2);
+
+    std::cout << "node1: " << node1;
+    std::cout << "node3: " << node3;
+
     return 0;
 }
