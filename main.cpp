a410691c5a3978e697e0202b6d7eb31b2c609c6b
Zhabitskaya Alexandra <yasashochek@gmail.com>
2024-10-09 19:17:00 +0300

    upd: new tests in main

--- main.cpp
+++ main.cpp
@@ -2,18 +2,41 @@
 #include <iostream>
 
 int main() {
-    TreeNode<int> node1(5);
-    TreeNode<int> node2(4);
-    node1.set_lchild(&node2);
+    int arr[] = {5, 3, 7, 6, 10, 1, 3, 8, 4, 9};
+    BinTree<int> tree1(arr, 10);
+    tree1.add(0);
+    std::cout << "Init tree:" << std::endl << tree1;
 
-    TreeNode<int> node3(1);
-    node3.set_lchild(&node2);
-
-    std::cout << "node1: " << node1;
-    std::cout << "node3: " << node3;
-
-    BinTree<int> tree1;
     BinTree<int> tree2 = tree1;
+    std::cout << "Copied tree:" << std::endl << tree2;
+
+    auto it = tree1.find(8);
+    if (it != nullptr) {
+        std::cout << "Found value: " << *it;
+
+        auto next = tree1.get_next(it);
+        if (next)
+            std::cout << "Next: " << *next;
+
+        auto prev = tree1.get_prev(it);
+        if (prev)
+            std::cout << "Prev: " << *prev;
+    }
+
+    std::cout << std::boolalpha << tree1.contains(12) << " " << tree1.contains(6) << std::endl;
+
+    tree1.remove(1);
+    std::cout << "Removed 1:" << std::endl
+              << tree1;
+
+    tree1.remove(9);
+    std::cout << "Removed 9:" << std::endl
+              << tree1;
+
+    if (tree1.min() != nullptr)
+        std::cout << "Min: " << *tree1.min();
+    if (tree1.max() != nullptr)
+        std::cout << "Max: " << *tree1.max();
 
     return 0;
 }
