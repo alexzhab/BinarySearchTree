15ed7677d97fdb73ae11766f597427c5f739060e
Zhabitskaya Alexandra <yasashochek@gmail.com>
2024-10-21 22:53:28 +0300

    add: class Multiset with Comparator struct

--- SetMap.h
+++ SetMap.h
@@ -44,21 +44,49 @@ public:
     typename BinTree<T, Functor>::ConstIterator& operator--() = delete;
     typename BinTree<T, Functor>::ConstIterator& operator--(int) = delete;
 };
+
+template<typename T, typename Functor>
+struct PairCompare {
+    bool operator()(const Pair<T, int>& t1, const Pair<T, int>& t2) const {
+        return Functor(t1.get_first(), t2.get_first());
+    }
+};
+
+template<typename T, typename Functor>
+class Multiset {
 private:
-    BinTree<T, Functor>* m_tree;
+    BinTree<Pair<T, int>, PairCompare<T, Functor>>* m_tree;
 
 public:
-    Set(BinTree<T, Functor>* tree) 
-    : m_tree(tree) {
+    Multiset() {
+        m_tree = new BinTree<Pair<T, int>, Functor>();
     }
-    ~Set() = default;
+    ~Multiset() = default;
+
+    BinTree<Pair<T, int>, Functor>* get_tree() const {
+        return m_tree;
+    }
 
     void add(const T& data) {
         m_tree->add(data);
     }
-
     bool contains(const T& data) const {
         return m_tree->contains(data);
     }
+
+    friend std::ostream& operator<<(std::ostream& os, const Multiset<T, Functor>& mset) {
+        os << *mset.get_tree() << std::endl;
+        return os;
+    }
+
+    typename BinTree<T, Functor>::ConstIterator cbegin() const {
+        return m_tree->cbegin();
+    }
+    typename BinTree<T, Functor>::ConstIterator cend() const {
+        return m_tree->cend();
+    }
+    typename BinTree<T, Functor>::ConstIterator& operator--() = delete;
+    typename BinTree<T, Functor>::ConstIterator& operator--(int) = delete;
+};
 
 #endif // SET_MAP_H
